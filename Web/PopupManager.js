0aa71393be92fd5cd01de6e7e2b824d4455e0403
= <luigi.naudi@gmail.com>
2024-06-21 02:32:12 +0200

    Popup working for both heatmaps

--- Web/PopupManager.js
+++ Web/PopupManager.js
@@ -68,22 +68,21 @@ class PopupManager {
         const pixel = this.map.getEventPixel(evt.originalEvent);
         const feature = this.map.forEachFeatureAtPixel(pixel, feature => feature);
     
-        // Check if the Predictions Heatmap Layer is visible and the featureInfoFlag is true
+        // Check if the Predictions Heatmap or the Historical Data Heatmap Layer is visible and the featureInfoFlag is true
         const predictionsLayer = this.map.getLayers().getArray().find(layer => layer.get('title') === 'Predictions Heatmap');
-        if (predictionsLayer && predictionsLayer.getVisible() && this.featureInfoFlag) {
+        const historicalLayer = this.map.getLayers().getArray().find(layer => layer.get('title') === 'Historical Data Heatmap');
+
+        if ((predictionsLayer && predictionsLayer.getVisible() || historicalLayer && historicalLayer.getVisible()) && this.featureInfoFlag) {
             if (feature && feature.get('total_crimes')) {
                 const coordinate = evt.coordinate;
                 const totalCrimes = feature.get('total_crimes');
                 const areaName = feature.get('area_name');
                 const month = feature.get('month');
                 const crimes = feature.get('crimes');
-                const year = '2019'; // Always for predictions heatmap
-    
                 const htmlContent = `
                     <div class="popup-content">
                         <div class="popup-row"><h3>Area Name:</h3><p>${areaName}</p></div>
                         <div class="popup-row"><h3>Crimes:</h3><p>${crimes}</p></div>
-                        <div class="popup-row"><h3>Year:</h3><p>${year}</p></div>
                         <div class="popup-row"><h3>Month:</h3><p>${month}</p></div>
                         <div class="popup-row"><h3>Total Crimes:</h3><p>${totalCrimes}</p></div>
                     </div>`;
@@ -139,172 +138,156 @@ class PopupManager {
         }
     }
 
-    updateSimplePopupContent(htmlContent) {
-        this.simpleContent.innerHTML = htmlContent;
+    setupCloseEventHandler() {
+        this.closer.onclick = () => {
+            this.hidePopup();
+            this.closer.blur();
+            return false;
+        };
     }
 
-    showSimplePopup(coordinate, content = null) {
-        if (content) {
-            this.updateSimplePopupContent(content);
-        }
-        this.simplePopup.setPosition(coordinate);
-    }
-
-    hideSimplePopup() {
-        this.simplePopup.setPosition(undefined);
-    }
-    
-        setupCloseEventHandler() {
-            this.closer.onclick = () => {
-                this.hidePopup();
-                this.closer.blur();
-                return false;
-            };
-        }
-    
-        setupRemoveEventHandler() {
-            this.removeButton.onclick = () => {
-                this.hidePopup();
-                if (this.highlightOverlay) {
-                    this.map.removeOverlay(this.highlightOverlay);
-                    this.highlightOverlay = null;
-                }
-                this.closer.blur();
-                return false;
-            };
-        }
-    
-        toggleFeatureInfoFlag() {
-            this.featureInfoFlag = !this.featureInfoFlag;
-        }
-    
-        handleMapClick(evt) {
-            if (!this.featureInfoFlag) return;
-    
-            const viewResolution = this.map.getView().getResolution();
-            const crimesLayer = this.map.getLayers().getArray().find(layer => layer.get('title') === 'Crime');
-    
-            if (!crimesLayer || !crimesLayer.getVisible()) {
-                this.popup.setPosition(undefined);
-                return;
-            }
-    
-            const url = crimesLayer.getSource().getFeatureInfoUrl(
-                evt.coordinate,
-                viewResolution,
-                'EPSG:3857',
-                { 'INFO_FORMAT': 'application/json', 'propertyName': 'date_occ,area_name,premis_des,crm_cd_des,agg_des' }
-            );
-    
-            if (url) {
-                fetch(url)
-                    .then(response => response.json())
-                    .then(json => {
-                        if (json.features && json.features.length > 0) {
-                            const feature = json.features[0];
-                            const properties = feature.properties;
-                            const htmlContent = `<div class="popup-content-row"><h3>Date:</h3><p>${properties.date_occ}</p></div>
-                                                 <div class="popup-content-row"><h3>Crime Type:</h3><p>${properties.agg_des}</p></div>
-                                                 <div class="popup-content-row"><h3>Crime:</h3><p>${properties.crm_cd_des}</p></div>
-                                                 <div class="popup-content-row"><h3>Area:</h3><p>${properties.area_name}</p></div>
-                                                 <div class="popup-content-row"><h3>Premises:</h3><p>${properties.premis_des}</p></div>`;
-                            this.updatePopupContent(htmlContent);
-                            this.showPopup(evt.coordinate);
-                        } else {
-                            this.popup.setPosition(undefined);
-                        }
-                    })
-                    .catch(error => {
-                        console.error('Error fetching feature information:', error);
-                        this.updatePopupContent('<p>Error fetching information.</p>');
-                        this.showPopup(evt.coordinate);
-                    });
-            } else {
-                this.popup.setPosition(undefined);
-            }
-        }
-    
-        updatePopupContent(htmlContent) {
-            this.content.innerHTML = htmlContent;
-        }
-    
-        showPopup(coordinate, content = null) {
-            if (content) {
-                this.updatePopupContent(content);
-            }
-            this.popup.setPosition(coordinate);
-            this.popupShown = true;
-            this.addPoint(coordinate);
-        }
-    
-        hidePopup() {
-            this.popup.setPosition(undefined);
-            this.popupShown = false;
-            this.enablePointHover();
-        }
-    
-        addPoint(coordinate) {
+    setupRemoveEventHandler() {
+        this.removeButton.onclick = () => {
+            this.hidePopup();
             if (this.highlightOverlay) {
                 this.map.removeOverlay(this.highlightOverlay);
+                this.highlightOverlay = null;
             }
-    
-            const pointElement = document.createElement('div');
-            pointElement.className = 'popup-point';
-    
-            this.highlightOverlay = new ol.Overlay({
-                position: coordinate,
-                positioning: 'center-center',
-                element: pointElement,
-                stopEvent: false
-            });
-    
-            this.map.addOverlay(this.highlightOverlay);
-    
-            pointElement.addEventListener('click', () => {
-                if (this.popupShown) {
-                    this.hidePopup();
-                } else {
-                    this.showPopup(coordinate);
-                    this.disablePointHover();
-                }
-            });
-    
-            // Add hover effect to change the color to green
-            pointElement.addEventListener('mouseenter', () => {
-                if (!this.popupShown) {
-                    pointElement.style.backgroundColor = 'green';
-                }
-            });
-    
-            pointElement.addEventListener('mouseleave', () => {
-                if (!this.popupShown) {
-                    pointElement.style.backgroundColor = 'yellow';
-                }
-            });
+            this.closer.blur();
+            return false;
+        };
+    }
+
+    toggleFeatureInfoFlag() {
+        this.featureInfoFlag = !this.featureInfoFlag;
+    }
+
+    handleMapClick(evt) {
+        if (!this.featureInfoFlag) return;
+
+        const viewResolution = this.map.getView().getResolution();
+        const crimesLayer = this.map.getLayers().getArray().find(layer => layer.get('title') === 'Crime');
+
+        if (!crimesLayer || !crimesLayer.getVisible()) {
+            this.popup.setPosition(undefined);
+            return;
         }
-    
-        disablePointHover() {
-            if (this.highlightOverlay) {
-                const pointElement = this.highlightOverlay.getElement();
-                pointElement.removeEventListener('mouseenter', this.pointHoverEnter);
-                pointElement.removeEventListener('mouseleave', this.pointHoverLeave);
-            }
-        }
-    
-        enablePointHover() {
-            if (this.highlightOverlay) {
-                const pointElement = this.highlightOverlay.getElement();
-                pointElement.addEventListener('mouseenter', this.pointHoverEnter);
-                pointElement.addEventListener('mouseleave', this.pointHoverLeave);
-            }
-        }
-    
-        pointHoverEnter() {
-            this.style.backgroundColor = 'green';
-        }
-    
-        pointHoverLeave() {
-            this.style.backgroundColor = 'yellow';
+
+        const url = crimesLayer.getSource().getFeatureInfoUrl(
+            evt.coordinate,
+            viewResolution,
+            'EPSG:3857',
+            { 'INFO_FORMAT': 'application/json', 'propertyName': 'date_occ,area_name,premis_des,crm_cd_des,agg_des' }
+        );
+
+        if (url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(json => {
+                    if (json.features && json.features.length > 0) {
+                        const feature = json.features[0];
+                        const properties = feature.properties;
+                        const htmlContent = `<div class="popup-content-row"><h3>Date:</h3><p>${properties.date_occ}</p></div>
+                                             <div class="popup-content-row"><h3>Crime Type:</h3><p>${properties.agg_des}</p></div>
+                                             <div class="popup-content-row"><h3>Crime:</h3><p>${properties.crm_cd_des}</p></div>
+                                             <div class="popup-content-row"><h3>Area:</h3><p>${properties.area_name}</p></div>
+                                             <div class="popup-content-row"><h3>Premises:</h3><p>${properties.premis_des}</p></div>`;
+                        this.updatePopupContent(htmlContent);
+                        this.showPopup(evt.coordinate);
+                    } else {
+                        this.popup.setPosition(undefined);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching feature information:', error);
+                    this.updatePopupContent('<p>Error fetching information.</p>');
+                    this.showPopup(evt.coordinate);
+                });
+        } else {
+            this.popup.setPosition(undefined);
         }
     }
-    
+
+    updatePopupContent(htmlContent) {
+        this.content.innerHTML = htmlContent;
+    }
+
+    showPopup(coordinate, content = null) {
+        if (content) {
+            this.updatePopupContent(content);
+        }
+        this.popup.setPosition(coordinate);
+        this.popupShown = true;
+        this.addPoint(coordinate);
+    }
+
+    hidePopup() {
+        this.popup.setPosition(undefined);
+        this.popupShown = false;
+        this.enablePointHover();
+    }
+
+    addPoint(coordinate) {
+        if (this.highlightOverlay) {
+            this.map.removeOverlay(this.highlightOverlay);
+        }
+
+        const pointElement = document.createElement('div');
+        pointElement.className = 'popup-point';
+
+        this.highlightOverlay = new ol.Overlay({
+            position: coordinate,
+            positioning: 'center-center',
+            element: pointElement,
+            stopEvent: false
+        });
+
+        this.map.addOverlay(this.highlightOverlay);
+
+        pointElement.addEventListener('click', () => {
+            if (this.popupShown) {
+                this.hidePopup();
+            } else {
+                this.showPopup(coordinate);
+                this.disablePointHover();
+            }
+        });
+
+        // Add hover effect to change the color to green
+        pointElement.addEventListener('mouseenter', () => {
+            if (!this.popupShown) {
+                pointElement.style.backgroundColor = 'green';
+            }
+        });
+
+        pointElement.addEventListener('mouseleave', () => {
+            if (!this.popupShown) {
+                pointElement.style.backgroundColor = 'yellow';
+            }
+        });
+    }
+
+    disablePointHover() {
+        if (this.highlightOverlay) {
+            const pointElement = this.highlightOverlay.getElement();
+            pointElement.removeEventListener('mouseenter', this.pointHoverEnter);
+            pointElement.removeEventListener('mouseleave', this.pointHoverLeave);
+        }
+    }
+
+    enablePointHover() {
+        if (this.highlightOverlay) {
+            const pointElement = this.highlightOverlay.getElement();
+            pointElement.addEventListener('mouseenter', this.pointHoverEnter);
+            pointElement.addEventListener('mouseleave', this.pointHoverLeave);
+        }
+    }
+
+    pointHoverEnter() {
+        this.style.backgroundColor = 'green';
+    }
+
+    pointHoverLeave() {
+        this.style.backgroundColor = 'yellow';
+    }
+}
